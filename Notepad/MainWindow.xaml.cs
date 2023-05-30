764fa065143f1fc3b2d56761462ae6cb19d1952c
kryman27 <77673262+kryman27@users.noreply.github.com>
2023-05-30 22:14:46 +0200

    Saving File Dialog added to Saving to file window, temperature from open-meteo added to main window

--- Notepad/MainWindow.xaml.cs
+++ Notepad/MainWindow.xaml.cs
@@ -1,80 +1,97 @@
-﻿using Notepad.Model;
+﻿using Newtonsoft.Json;
+using Notepad.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.EntityFrameworkCore;
 
 namespace Notepad
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            string text = NoteText.Text;
-            Saving formB = new Saving(text);
-            formB.Show();
-        }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		private void Save_Click(object sender, RoutedEventArgs e)
+		{
+			string text = NoteText.Text;
+			Saving formB = new Saving(text);
+			formB.Show();
+		}
 
-        private void NoteText_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (NoteText.Text == "Enter your thoughts here...")
-            {
-                NoteText.Clear();
-            }
-        }
+		private void NoteText_GotFocus(object sender, RoutedEventArgs e)
+		{
+			if (NoteText.Text == "Enter your thoughts here...")
+			{
+				NoteText.Clear();
+			}
+		}
 
-        private void Clear_Click(object sender, RoutedEventArgs e)
-        {
-            NoteText.Clear();
-        }
+		private void Clear_Click(object sender, RoutedEventArgs e)
+		{
+			NoteText.Clear();
+		}
 
-        private void btnSaveToDatabase_Click(object sender, RoutedEventArgs e)
-        {
-            using (var ctxNotes = new NotesContext())
-            {
-                ctxNotes.Notes.Add(new Model.Note
-                {
-                    Note1 = NoteText.Text,
-                    CreatedOn= DateTime.Now,
-                });
-                ctxNotes.SaveChanges();
-                var resOfAdd = true;
-                
-                if(resOfAdd == true)
-                {
-                    MessageBox.Show("Note stored to DB sucesfully");
-                }
-                else if(resOfAdd == false)
-                {
-                    MessageBox.Show("Somethin wrong happened! Note was not stored!");
-                }
-                NoteText.Clear();
-            }
+		private void btnSaveToDatabase_Click(object sender, RoutedEventArgs e)
+		{
+			using (var ctxNotes = new NotesContext())
+			{
+				ctxNotes.Notes.Add(new Model.Note
+				{
+					Note1 = NoteText.Text,
+					CreatedOn = DateTime.Now,
+				});
+				ctxNotes.SaveChanges();
+				var resOfAdd = true;
 
-        }
+				if (resOfAdd == true)
+				{
+					MessageBox.Show("Note stored to DB sucesfully");
+				}
+				else if (resOfAdd == false)
+				{
+					MessageBox.Show("Somethin wrong happened! Note was not stored!");
+				}
+				NoteText.Clear();
+			}
 
-        private void ShowNotes_Click(object sender, RoutedEventArgs e)
-        {
-            NotesList listOfNotes = new NotesList();
-            listOfNotes.Show();
-        }
-    }
+		}
+
+		private void ShowNotes_Click(object sender, RoutedEventArgs e)
+		{
+			NotesList listOfNotes = new NotesList();
+			listOfNotes.Show();
+		}
+
+		private async void Temperature_Loaded(object sender, RoutedEventArgs e)
+		{
+			//string jsonWeatherData = String.Empty;
+
+			using (HttpClient client = new HttpClient())
+			{
+
+				string weatherApiUrl = "https://api.open-meteo.com/v1/forecast?latitude=51.25&longitude=22.57&hourly=temperature_2m&forecast_days=1";
+
+
+				try
+				{
+					HttpResponseMessage response = await client.GetAsync(weatherApiUrl);
+					string rawWeatherData = await response.Content.ReadAsStringAsync();
+
+					WeatherData finalData = JsonConvert.DeserializeObject<WeatherData>(rawWeatherData);
+
+					Temperature.Text = finalData.hourly.temperature_2m[0].ToString() + "°C";
+				}
+				catch (Exception ex)
+				{
+					Temperature.Text = ex.Message;
+				}
+			}
+
+
+		}
+	}
 }
---
+++ Notepad/WeatherData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad
+{
+	public class WeatherData
+	{
+		public DailyData hourly { get; set; }
+	}
+
+	public class DailyData
+	{
+		public List<string> time { get; set; }
+		public List<double> temperature_2m { get; set; }
+	}
+}
